74c18f808903026e3e8b606a62be734949a88d66
Jose Sosa <josesosa@Joses-MacBook-Pro.local>
2025-03-13 20:14:33 -0600

    change logo url in mochita page

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from 'next/router';
 import SocialLinks from '../SocialLinks/socialLinks';
 import ProfileIcon from "../../containers/ProfileIcon/profileIcon";
 import styles from "./header.module.css";
 
 const Header = () => {
+  const router = useRouter();
+  const isMochitaPage = router.pathname.startsWith('/mochita');
+  const homeLink = isMochitaPage ? '/mochita' : '/';
+
   return (
     <header className={styles.header}>
       <nav>
-        <Link href="/" className={styles.logo}>
+        <Link href={homeLink} className={styles.logo}>
           <Image
             src="/logo.jpeg"
             alt="logo"
